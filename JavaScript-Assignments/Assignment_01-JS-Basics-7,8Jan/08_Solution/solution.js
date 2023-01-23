0344bf71c0bd3b917f9c336ebef66637efc2ffd9
madhavsahi <madhavsahi16@gmail.com>
2023-01-24 03:55:58 +0530

    n

--- JavaScript-Assignments/Assignment_01-JS-Basics-7,8Jan/08_Solution/solution.js
+++
@@ -1,7 +0,0 @@
- let ans=" MadhavSahi    ";
- 
- console.log(ans);
- console.log(ans.length);
-
- console.log(ans.trim());
- console.log(ans.trim().length);
--- JavaScript-Assignments/Assignment_01-JS-Basics-7,8Jan/29_Solution/solution.js
+++
@@ -1,9 +0,0 @@
-let filename="javascript.html";
-
-fun=(file1)=>{
-    let x = file1.lastIndexOf('.');
-    let ans=file1.split(file1[x]);
-    console.log("Extension of input file is : " + ans[ans.length-1]);
-}
-
-fun(filename);
