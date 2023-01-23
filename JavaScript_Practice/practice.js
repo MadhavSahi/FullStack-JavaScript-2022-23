3ab8eaf460beeee3bc9d9e9f092c323abf8ed196
madhavsahi <madhavsahi16@gmail.com>
2023-01-23 18:47:41 +0530

    new changes

---
+++ JavaScript_Practice/practice.js
@@ -0,0 +1,53 @@
+// let variable1=document.getElementsByClassName("followers");
+// let variable2=document.querySelector(".num");
+// let var3=document.getElementsByTagName("body");
+// let var4=document.querySelector(".third");
+// // console.log(variable);
+// let count=1;
+// setInterval(()=>{ // itne miliseconds baad left vala function krte rho continuously..Use if statement to stop
+//     if(count<1000){
+//         count++;
+//         variable2.innerHTML=count;
+//     }
+// },1);
+// setTimeout(()=>{// itne miliseconds baad left vala function krdo 1 baar
+//     variable1[0].innerHTML+= " of Instagram";
+//     // var3[0].innerHTML="s";    
+// },4650)
+
+// let pink=document.querySelector(".pink");
+// let red=document.querySelector(".red");
+// let orange=document.querySelector(".orange");
+// let black=document.querySelector(".black");
+
+// let bgcolorpicker=(pickedelement)=>{
+//     return window.getComputedStyle(pickedelement).backgroundColor;
+// }
+// // let ans=bgcolorpicker(black);
+// // console.log(ans);
+
+// addBGCOLOR=(element,color)=>{
+//         element.addEventListener('mouseenter',()=>{
+//              var4.style.background=color;
+//     })
+// }
+// addBGCOLOR(pink,bgcolorpicker(pink));
+// addBGCOLOR(red,bgcolorpicker(red));
+// addBGCOLOR(orange,bgcolorpicker(orange));
+// addBGCOLOR(black,bgcolorpicker(black));
+
+// // pink.addEventListener('mouseleave',()=>{
+// //     return var4.style.background=bgcolorpicker(var4);
+// // })
+console.log(window.document.body);
+// answer=()=>{
+//     var hey="hii";
+//     console.log(hey);
+// }
+// answer();
+// answer();
+// answer();
+// let fxn=answer();
+// console.log(fxn());
+// fxn.answer();
+// console.log(answer());
