508865bcc535c791228005eab6e5a731d5fc766f
madhavsahi <madhavsahi16@gmail.com>
2023-01-24 15:28:00 +0530

    added dom files

---
+++ Class-16,17-DOM_21,22-Jan_23/DOM-Basics/script.js
@@ -0,0 +1,12 @@
+var ul=document.querySelector(".second");
+
+var div=document.querySelector(".first");
+//creating and appending HTML elements using DOM.
+var li=document.createElement("li");
+div.appendChild(li);
+// ul.appendChild(li);
+li.className="hello";
+li.style.backgroundColor="grey";
+li.style.color="purple";
+li.style.fontSize="50px";
+li.textContent="Hello world";
