df855c0d4ec1846f18abb09d5ac37a4e042dda73
madhavsahi <madhavsahi16@gmail.com>
2023-01-24 15:35:41 +0530

    added dom files

--- Class-16,17-DOM_21,22-Jan_23/DOM-Basics/script.js
+++ Class-16,17-DOM_21,22-Jan_23/DOM-Basics/script.js
@@ -10,3 +10,5 @@ li.style.backgroundColor="grey";
 li.style.color="purple";
 li.style.fontSize="50px";
 li.textContent="Hello world";
+div.removeChild(ul);// to remove HTML element 
+// document.body.removeChild(div);
