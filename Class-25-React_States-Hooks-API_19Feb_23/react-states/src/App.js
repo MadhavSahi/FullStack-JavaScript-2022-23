a4699389c08fe520626783b6cda3fd599307a799
madhavsahi <madhavsahi16@gmail.com>
2023-02-23 23:00:59 +0530

    updated files

--- Class-25-React_States-Hooks-API_19Feb_23/react-states/src/App.js
+++ Class-25-React_States-Hooks-API_19Feb_23/react-states/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { ReactDOM } from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  let [count,setCount]=useState(0);//using state
+  let add=()=>{
+    
+    setCount(count+1);//updating the value using state
+
+    // console.log(count);
+  };//count is updated after end of line 14.
+  // console.log(count);
+  
+  let sub=()=>{
+    if(count-1<0){
+      return setCount(100);
+      // return;
+    }
+    return setCount(count-1);
+    // console.log(count);
+  };
+  let reset=()=>{
+    return setCount(0);//we can write or ignore return for useState.
+    // console.log(count);
+  };
+   return(
+    <>
+      <h3>This is a counter App</h3>
+      <p>Count is {count}</p>
+      <button className="btn" onClick={add} >INCREASE THE COUNT</button>
+      <button className="btn" onClick={sub} >DECREASE THE COUNT</button>
+      <button className="btn" onClick={reset} >RESET THE COUNT</button>
+      {/* whens using state...we have to put setCount inside a function and not use it */}
+    </>
+   )
+};
 export default App;
+
+// - If use the component multiple time...then updation in 1 component won't effect the other components...it means that states are not shared..they are independent.
+//- For diff. components...the state is obviously 1...but multiple copies are created in the memory.
+//- Virtual DOM is basically creating the different copies of that state in the memory....so all the components are not sharing the same state values.
--- Class-25-React_States-Hooks-API_19Feb_23/react-states/src/index.js
+++ Class-25-React_States-Hooks-API_19Feb_23/react-states/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App';//the CSS of App will have preference over index bcz it is below
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  <>
+    <App></App>
+    {/* <App></App> */}
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
