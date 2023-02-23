eec4dd8703ec5a49e65041b3ebecae2ec53e0ce0
madhavsahi <madhavsahi16@gmail.com>
2023-02-23 20:30:40 +0530

    added class folder

---
+++ Class-24-React_Basics_02-18thFeb_23/react02-basics/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// const hello=()=>{
+//   return React.createElement(
+//     <>
+//       <p>Hey Madhav how's you?</p>
+//     </>
+//   );
+// };
+// ReactDOM.render(<hello />,document.getElementById("root"));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
