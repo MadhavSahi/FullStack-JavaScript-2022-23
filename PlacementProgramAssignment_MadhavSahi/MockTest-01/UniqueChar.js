2c4301f0209f5f04f9d9a416e44193aa0f8e4735
madhavsahi <madhavsahi16@gmail.com>
2023-06-05 23:59:42 +0530

    added sol

--- PlacementProgramAssignment_MadhavSahi/MockTest-01/UniqueChar.js
+++ PlacementProgramAssignment_MadhavSahi/MockTest-01/UniqueChar.js
@@ -19,3 +19,25 @@
 // b. s consists of only lowercase English letters.
 
 //Solution--->
+
+function firstUniqChar(s) {
+  let charCount = {}; // Object to store the count of each character
+
+  // Count the occurrences of each character
+  for (let char of s) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  // Find the first non-repeating character and return its index
+  for (let i = 0; i < s.length; i++) {
+    if (charCount[s[i]] === 1) {
+      return i;
+    }
+  }
+
+  // No non-repeating characters found
+  return -1;
+}
+console.log(firstUniqChar("leetcode")); // Output: 0
+console.log(firstUniqChar("loveleetcode")); // Output: 2
+console.log(firstUniqChar("aabb")); // Output: -1
