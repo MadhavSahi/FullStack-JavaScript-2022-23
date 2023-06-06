34e811cf25e4fc0a27e2ee154fa37f850ae08505
madhavsahi <madhavsahi16@gmail.com>
2023-06-06 23:54:08 +0530

    added todo app

--- PlacementAssignment_MadhavSahi/React-Solutions/todo-react/src/Form.js
+++ PlacementAssignment_MadhavSahi/React-Solutions/todo-react/src/Form.js
@@ -1,79 +1,88 @@
-import React,{useState} from 'react';
-import "./Form.css";
+import React, { useState, useReducer } from 'react';
+import './Form.css';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, action.payload];
+    case 'DELETE_TODO':
+      return state.filter((_, index) => index !== action.payload);
+    case 'DELETE_ALL_TODOS':
+      return [];
+    default:
+      return state;
+  }
+}
 
 function Form() {
-
-  const [text,setText]=useState("");
-  const [todos,setTodos]=useState([]);
+  const [text, setText] = useState('');
+  const [todos, dispatch] = useReducer(reducer, []);
 
   const classs = {
-    color: "white",
-    backgroundColor: "DodgerBlue",
-    padding: "10px",
-    fontFamily: "Arial"
+    color: 'white',
+    backgroundColor: 'DodgerBlue',
+    padding: '10px',
+    fontFamily: 'Arial',
   };
 
-  const changetext=(event)=>{
+  const handleChangeText = (event) => {
     setText(event.target.value);
   };
 
-  const handlesubmit=()=>{
-      setTodos((todos)=>{
-        const newtodos=[...todos,text];
-        setText("");
-        return newtodos;
-
-      });
+  const handleSubmit = () => {
+    if (text.trim() !== '') {
+      dispatch({ type: 'ADD_TODO', payload: text });
+      setText('');
+    }
   };
 
-  const handledelete=(i)=>{
-    const newtodos=todos.filter((val,id)=>{
-        return (i!==id);
-    })
-    setTodos(newtodos);
+  const handleDelete = (index) => {
+    dispatch({ type: 'DELETE_TODO', payload: index });
   };
 
-  const handledeleteall=()=>{
-    setTodos([]) 
+  const handleDeleteAll = () => {
+    dispatch({ type: 'DELETE_ALL_TODOS' });
   };
 
-  
   return (
     <>
-        <div className='formdiv'>
-            <label className='formlabel'>Enter Task :-
-                <input
-                    className='forminput'
-                    onChange={changetext}
-                    type="text" 
-                    placeholder='Enter Task of the Day'
-                    value={text}                
-                >            
-                </input>
-            </label>
-            <button className='formbtn' onClick={handlesubmit}>SUBMIT</button>
+      <div className="formdiv">
+        <label className="formlabel">
+          Enter Task:
+          <input
+            className="forminput"
+            onChange={handleChangeText}
+            type="text"
+            placeholder="Enter Task of the Day"
+            value={text}
+          />
+        </label>
+        <button className="formbtn" onClick={handleSubmit}>
+          SUBMIT
+        </button>
+      </div>
+
+      <ul>
+        {todos.length > 0 &&
+          todos.map((val, i) => (
+            <div key={i} className="ul-li">
+              <li className={`li ${classs}`}>{val}</li>
+              <button onClick={() => handleDelete(i)} className="deleted">
+                Delete
+              </button>
+            </div>
+          ))}
+      </ul>
+
+      {todos.length >= 2 && (
+        <div className="deleteallbutton">
+          <button className="deleteallbutton-1" onClick={handleDeleteAll}>
+            DELETE ALL Tasks
+          </button>
         </div>
-       
-        <ul>
-            {todos!==[] && todos.map((val,i)=>{
-                return (
-                    <div key={i} className='ul-li'>
-                        <li className={`li ${classs}`}>{val}</li>
-                        <button onClick={()=>handledelete(i)} className='deleted'>Delete</button>
-                    </div>
-                )
-            })}
-        </ul>
-        {todos.length>=2 && 
-        <>
-          <div className='deleteallbutton'>
-              <button className='deleteallbutton-1' onClick={handledeleteall} >DELETE ALL Tasks</button>
-          </div>
-        </>
-        }
-        
+      )}
     </>
-  )
-};
+  );
+}
 
 export default Form;
