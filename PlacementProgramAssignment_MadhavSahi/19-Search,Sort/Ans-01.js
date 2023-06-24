7cfb47eed12219cda4fc234644bb9a039db183a0
madhavsahi <madhavsahi16@gmail.com>
2023-06-24 23:59:31 +0530

    added assignment 19 search sorts

--- PlacementProgramAssignment_MadhavSahi/19-Search,Sort/Ans-01.js
+++ PlacementProgramAssignment_MadhavSahi/19-Search,Sort/Ans-01.js
@@ -34,3 +34,73 @@
 // - The sum of `lists[i].length` will not exceed `10000`.
 
 // Solution--->
+function mergeSortedLists(lists) {
+  if (lists.length === 0) {
+    return null;
+  }
+
+  while (lists.length > 1) {
+    const mergedLists = [];
+
+    for (let i = 0; i < lists.length; i += 2) {
+      const listA = lists[i];
+      const listB = lists[i + 1];
+      const mergedList = mergeTwoLists(listA, listB);
+      mergedLists.push(mergedList);
+    }
+
+    lists = mergedLists;
+  }
+
+  return lists[0];
+}
+
+function mergeTwoLists(listA, listB) {
+  const dummy = new ListNode(0);
+  let current = dummy;
+
+  while (listA !== null && listB !== null) {
+    if (listA.val < listB.val) {
+      current.next = listA;
+      listA = listA.next;
+    } else {
+      current.next = listB;
+      listB = listB.next;
+    }
+
+    current = current.next;
+  }
+
+  if (listA !== null) {
+    current.next = listA;
+  }
+
+  if (listB !== null) {
+    current.next = listB;
+  }
+
+  return dummy.next;
+}
+
+class ListNode {
+  constructor(val = 0, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+const lists1 = [
+  new ListNode(1, new ListNode(4, new ListNode(5))),
+  new ListNode(1, new ListNode(3, new ListNode(4))),
+  new ListNode(2, new ListNode(6)),
+];
+console.log(mergeSortedLists(lists1));
+// Output: [1,1,2,3,4,4,5,6]
+
+const lists2 = [];
+console.log(mergeSortedLists(lists2));
+// Output: []
+
+const lists3 = [[]];
+console.log(mergeSortedLists(lists3));
+// Output: []
