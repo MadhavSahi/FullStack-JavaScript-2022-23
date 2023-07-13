d376b1f8606cd30af6d8dab1e8f5d813af4ce304
madhavsahi <madhavsahi16@gmail.com>
2023-07-14 00:31:24 +0530

    added solutions for webdev-07

--- PlacementProgramAssignment_MadhavSahi/WebDev-07/Ans-03/script.js
+++ PlacementProgramAssignment_MadhavSahi/WebDev-07/Ans-03/script.js
@@ -1,152 +1,10 @@
-// Header.js
-function createHeader() {
-    const header = document.createElement("div");
-    header.className = "flex flex-row justify-between bg-black";
-  
-    const titleLink = document.createElement("p");
-    titleLink.className = "mt-3 text-blue-300 text-2xl ml-10";
-    const title = document.createElement("a");
-    title.href = "/";
-    title.textContent = "GitHub User Finder App by Madhav Sahi";
-    titleLink.appendChild(title);
-  
-    const author = document.createElement("p");
-    author.className = "text-white mt-5 text-2xl mr-10";
-    author.textContent = "MADHAV SAHI";
-  
-    header.appendChild(titleLink);
-    header.appendChild(author);
-  
-    return header;
-  }
-  
-  // Footer.js
-  function createFooter() {
-    const footer = document.createElement("div");
-    footer.className = "flex flex-row justify-around flex-wrap bg-black sticky mt-[400px] bottom-0 w-full";
-  
-    const createdBy = document.createElement("p");
-    createdBy.className = "text-white text-3xl p-5";
-    createdBy.textContent = 'Created with Love By : © "Madhav Sahi"';
-  
-    const socialIcons = document.createElement("div");
-    socialIcons.className = "flex flex-row justify-around";
-  
-    const linkedinIcon = createSocialIcon("https://www.linkedin.com/in/madhav-sahi-6a2305161/", "BsLinkedin");
-    const githubIcon = createSocialIcon("https://twitter.com/Madhavvv_", "BsGithub");
-    const twitterIcon = createSocialIcon("https://github.com/MadhavSahi", "BsTwitter");
-    const hashnodeIcon = createSocialIcon("https://madhavsahi.hashnode.dev/", "SiHashnode");
-  
-    socialIcons.appendChild(linkedinIcon);
-    socialIcons.appendChild(githubIcon);
-    socialIcons.appendChild(twitterIcon);
-    socialIcons.appendChild(hashnodeIcon);
-  
-    footer.appendChild(createdBy);
-    footer.appendChild(socialIcons);
-  
-    return footer;
-  }
-  
-  function createSocialIcon(link, iconClass) {
-    const iconLink = document.createElement("a");
-    iconLink.target = "_blank";
-    iconLink.rel = "noreferrer";
-    iconLink.href = link;
-    iconLink.className = "text-white text-2xl mr-5 mt-1 p-5 hover:scale-x-125 hover:scale-y-105";
-  
-    const icon = document.createElement("i");
-    icon.className = iconClass;
-  
-    iconLink.appendChild(icon);
-  
-    return iconLink;
-  }
-  
-  // PageNotFound.js
-  function createPageNotFound() {
-    const notFound = document.createElement("p");
-    notFound.textContent = "Page not found";
-  
-    return notFound;
-  }
-  
-  // UserCard.js
-  function createUserCard(userDetails) {
-    const userCard = document.createElement("div");
-    userCard.className = "flex flex-col items-center mt-10 gap-3";
-  
-    const userImage = document.createElement("img");
-    userImage.className = "rounded-lg h-60 w-60";
-    userImage.alt = "user-avatar";
-    userImage.src = userDetails.avatar_url;
-  
-    const userName = createUserInfo("bg-cyan-500 px-5 text-2xl text-black", userDetails.name);
-    const userLocation = createUserInfo("bg-yellow-300 px-5 text-2xl text-black", userDetails.location);
-    const userBio = createUserInfo("bg-red-400 px-5 text-2xl text-black", userDetails.bio);
-    const userHireable = createUserInfo("text-2xl text-black", "Is Available for Hiring ? : " + (userDetails.hireable ? "Yes" : "No"));
-    const userFollowers = createUserInfo("text-2xl text-black", "Number of Followers : " + userDetails.followers);
-    const userFollowing = createUserInfo("text-2xl text-black", "Number of Followings : " + userDetails.following);
-    const userRepos = createUserInfo("text-2xl text-black", "Number of Public Repos : " + userDetails.public_repos);
-  
-    userCard.appendChild(userImage);
-    userCard.appendChild(userName);
-    userCard.appendChild(userLocation);
-    userCard.appendChild(userBio);
-    userCard.appendChild(userHireable);
-    userCard.appendChild(userFollowers);
-    userCard.appendChild(userFollowing);
-    userCard.appendChild(userRepos);
-  
-    return userCard;
-  }
-  
-  function createUserInfo(className, text) {
-    const info = document.createElement("p");
-    info.className = className;
-    info.textContent = text;
-  
-    return info;
-  }
-  
-  // UserRepos.js
-  function createUserRepos(userRepos, userDetails) {
-    const reposContainer = document.createElement("div");
-    reposContainer.className = "flex flex-col items-center gap-5 mt-10";
-  
-    const reposTitle = createUserInfo("bg-pink-400 px-5 mb-5 text-3xl text-black", userDetails.name + " REPOS :-");
-    reposContainer.appendChild(reposTitle);
-  
-    userRepos.forEach((repo) => {
-      const repoContainer = document.createElement("div");
-      repoContainer.className = "flex flex-col items-center mt-5";
-  
-      const repoLink = document.createElement("a");
-      repoLink.href = "https://github.com/" + repo.full_name;
-      repoLink.target = "_blank";
-      repoLink.rel = "noreferrer";
-  
-      const repoName = createUserInfo("bg-cyan-500 font-semibold px-5 text-2xl text-black", repo.name);
-      repoLink.appendChild(repoName);
-  
-      repoContainer.appendChild(repoLink);
-      reposContainer.appendChild(repoContainer);
-    });
-  
-    return reposContainer;
-  }
-  
-  // App.js
-  function createApp() {
+// App.js
+function createApp() {
     const appContainer = document.createElement("div");
   
     const header = createHeader();
-    const pageContent = document.createElement("div");
+    const pageContent = createPageContent();
     const footer = createFooter();
-  
-    const homePage = createHomePage();
-  
-    pageContent.appendChild(homePage);
   
     appContainer.appendChild(header);
     appContainer.appendChild(pageContent);
@@ -155,62 +13,214 @@ function createHeader() {
     return appContainer;
   }
   
-  // Home.js
-function createHomePage() {
-  const homePageContainer = document.createElement("div");
-  homePageContainer.className = "flex flex-col items-center home-page";
-
-  const searchSection = createSearchSection();
-  homePageContainer.appendChild(searchSection);
-
-  return homePageContainer;
-}
-
-function createSearchSection() {
-  const searchSection = document.createElement("div");
-  searchSection.className = "flex flex-row justify-center gap-3 mt-40 p-2";
-
-  const searchLabel = document.createElement("p");
-  searchLabel.className = "text-2xl text-black bg-gray-400 p-3";
-  searchLabel.textContent = "Enter GitHub User Name :";
-
-  const searchInput = document.createElement("input");
-  searchInput.className = "text-2xl text-black border-2 pl-5 border-black";
-  searchInput.type = "text";
-  searchInput.placeholder = "Enter UserName";
-
-  const submitButton = document.createElement("button");
-  submitButton.className = "px-2 text-lg text-white bg-blue-600 hover:scale-95 font-semibold rounded-md";
-  submitButton.textContent = "SUBMIT";
-  submitButton.addEventListener("click", fetchDetails);
-
-  searchSection.appendChild(searchLabel);
-  searchSection.appendChild(searchInput);
-  searchSection.appendChild(submitButton);
-
-  return searchSection;
-}
-
-async function fetchDetails() {
-  const query = document.querySelector("input").value;
-  try {
-    const response = await axios.get(`https://api.github.com/users/${query}`);
-    const data = response.data;
-    const userCard = createUserCard(data);
-    const userRepos = createUserRepos(data.repos, data);
-    const homePageContainer = document.querySelector(".home-page");
-    homePageContainer.innerHTML = "";
-    homePageContainer.appendChild(userCard);
-    homePageContainer.appendChild(userRepos);
-  } catch (error) {
-    console.error(error);
-    alert("No user found for this username");
+  // Header.js
+  function createHeader() {
+    const header = document.createElement("header");
+    header.className = "header";
+  
+    const logo = document.createElement("p");
+    logo.className = "logo";
+    logo.textContent = "GitHub User Finder App by Madhav Sahi";
+  
+    const author = document.createElement("p");
+    author.className = "author";
+    author.textContent = "MADHAV SAHI";
+  
+    header.appendChild(logo);
+    header.appendChild(author);
+  
+    return header;
   }
-}
-
-function renderApp() {
-  const root = document.getElementById("root");
-  root.appendChild(createApp());
-}
-
-renderApp();
+  
+  // Footer.js
+  function createFooter() {
+    const footer = document.createElement("footer");
+    footer.className = "footer";
+  
+    const createdBy = document.createElement("p");
+    createdBy.className = "created-by";
+    createdBy.textContent = 'Created with Love By: © "Madhav Sahi"';
+  
+    const socialLinks = document.createElement("div");
+    socialLinks.className = "social-links";
+  
+    const linkedinLink = createSocialLink("https://www.linkedin.com/in/madhav-sahi-6a2305161/", "linkedin");
+    const githubLink = createSocialLink("https://github.com/MadhavSahi", "github");
+    const twitterLink = createSocialLink("https://twitter.com/Madhavvv_", "twitter");
+    const hashnodeLink = createSocialLink("https://madhavsahi.hashnode.dev/", "hashnode");
+  
+    socialLinks.appendChild(linkedinLink);
+    socialLinks.appendChild(githubLink);
+    socialLinks.appendChild(twitterLink);
+    socialLinks.appendChild(hashnodeLink);
+  
+    footer.appendChild(createdBy);
+    footer.appendChild(socialLinks);
+  
+    return footer;
+  }
+  
+  function createSocialLink(link, iconName) {
+    const socialLink = document.createElement("a");
+    socialLink.href = link;
+    socialLink.target = "_blank";
+    socialLink.rel = "noreferrer";
+  
+    const icon = document.createElement("i");
+    icon.className = `fab fa-${iconName}`;
+  
+    socialLink.appendChild(icon);
+  
+    return socialLink;
+  }
+  
+  // PageContent.js
+  function createPageContent() {
+    const pageContent = document.createElement("div");
+    pageContent.className = "page-content";
+  
+    const home = createHome();
+  
+    pageContent.appendChild(home);
+  
+    return pageContent;
+  }
+  
+  // Home.js
+  function createHome() {
+    const home = document.createElement("div");
+    home.className = "home";
+  
+    const searchContainer = document.createElement("div");
+    searchContainer.className = "search-container";
+  
+    const searchInput = document.createElement("input");
+    searchInput.type = "text";
+    searchInput.placeholder = "Enter GitHub Username";
+    searchInput.className = "search-input";
+  
+    const searchButton = document.createElement("button");
+    searchButton.textContent = "Search";
+    searchButton.className = "search-button";
+  
+    const userContainer = document.createElement("div");
+    userContainer.className = "user-container";
+  
+    searchContainer.appendChild(searchInput);
+    searchContainer.appendChild(searchButton);
+  
+    home.appendChild(searchContainer);
+    home.appendChild(userContainer);
+  
+    searchButton.addEventListener("click", () => {
+      const username = searchInput.value.trim();
+      if (username !== "") {
+        fetchUserDetails(username)
+          .then((user) => {
+            userContainer.innerHTML = "";
+            userContainer.appendChild(createUserCard(user));
+            fetchUserRepos(user.repos_url)
+              .then((repos) => {
+                userContainer.appendChild(createUserRepos(repos));
+              })
+              .catch((error) => {
+                console.error(error);
+                alert("Failed to fetch user repositories");
+              });
+          })
+          .catch((error) => {
+            console.error(error);
+            alert("No user found for this username");
+          });
+      } else {
+        alert("Please enter a username");
+      }
+    });
+  
+    return home;
+  }
+  
+  // Fetch user details
+  async function fetchUserDetails(username) {
+    try {
+      const response = await axios.get(`https://api.github.com/users/${username}`);
+      return response.data;
+    } catch (error) {
+      throw new Error("Failed to fetch user details");
+    }
+  }
+  
+  // Fetch user repositories
+  async function fetchUserRepos(reposUrl) {
+    try {
+      const response = await axios.get(reposUrl);
+      return response.data;
+    } catch (error) {
+      throw new Error("Failed to fetch user repositories");
+    }
+  }
+  
+  // Create user card
+  function createUserCard(user) {
+    const userCard = document.createElement("div");
+    userCard.className = "user-card";
+  
+    const avatar = document.createElement("img");
+    avatar.src = user.avatar_url;
+    avatar.alt = "User Avatar";
+    avatar.className = "user-avatar";
+  
+    const username = document.createElement("h3");
+    username.textContent = user.login;
+    username.className = "username";
+  
+    const name = document.createElement("p");
+    name.textContent = user.name;
+    name.className = "name";
+  
+    const bio = document.createElement("p");
+    bio.textContent = user.bio;
+    bio.className = "bio";
+  
+    userCard.appendChild(avatar);
+    userCard.appendChild(username);
+    userCard.appendChild(name);
+    userCard.appendChild(bio);
+  
+    return userCard;
+  }
+  
+  // Create user repositories
+  function createUserRepos(repos) {
+    const reposContainer = document.createElement("div");
+    reposContainer.className = "repos-container";
+  
+    const reposTitle = document.createElement("h3");
+    reposTitle.textContent = "Repositories";
+    reposTitle.className = "repos-title";
+  
+    const reposList = document.createElement("ul");
+    reposList.className = "repos-list";
+  
+    repos.forEach((repo) => {
+      const repoItem = document.createElement("li");
+      repoItem.textContent = repo.name;
+      repoItem.className = "repo-item";
+  
+      reposList.appendChild(repoItem);
+    });
+  
+    reposContainer.appendChild(reposTitle);
+    reposContainer.appendChild(reposList);
+  
+    return reposContainer;
+  }
+  
+  // Render the app
+  function renderApp() {
+    const root = document.getElementById("root");
+    root.appendChild(createApp());
+  }
+  
+  renderApp();
+  
