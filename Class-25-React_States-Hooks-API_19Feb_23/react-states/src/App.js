b9f3c3244bae9800fae9cbdbdcd728f91706495b
madhavsahi <madhavsahi16@gmail.com>
2023-08-05 02:55:57 +0530

    practice js before job fair

--- Class-25-React_States-Hooks-API_19Feb_23/react-states/src/App.js
+++ Class-25-React_States-Hooks-API_19Feb_23/react-states/src/App.js
@@ -4,40 +4,61 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  let [count, setCount] = useState(0); //using state
+  let add = () => (setCount(count + 1)); //updating the value using state
 
-  let [count,setCount]=useState(0);//using state
-  let add=()=>{
-    
-    setCount(count+1);//updating the value using state
-
-    // console.log(count);
-  };//count is updated after end of line 14.
   // console.log(count);
-  
-  let sub=()=>{
-    if(count-1<0){
-      return setCount(100);
-      // return;
+  //count is updated after end of line 14.
+  // console.log(count);
+
+  // let sub = () => {
+  //   if (count - 1 < 0) {
+  //      return setCount(100);
+  //     // return;
+  //   }
+  //    setCount(count - 1);
+  //   // console.log(count);
+  // };
+  let sub = () => {
+    if (count - 1 < 0) {
+      setCount(100); // No need for return keyword here
+    } else {
+      setCount(count - 1);
     }
-    return setCount(count-1);
+  };
+  
+  let reset = () => {
+    return setCount(0); //we can write or ignore return for useState.
     // console.log(count);
   };
-  let reset=()=>{
-    return setCount(0);//we can write or ignore return for useState.
-    // console.log(count);
-  };
-   return(
+  return (
     <>
       <h3>This is a counter App</h3>
       <p>Count is {count}</p>
-      <button className="btn" onClick={()=>add()} >INCREASE THE COUNT</button>
-      <button className="btn" onClick={()=>{sub()}} >DECREASE THE COUNT</button>
-      <button className="btn" onClick={reset} >RESET THE COUNT</button>
+      <button
+        className="btn"
+        onClick={() => {
+          return add();
+        }}
+      >
+        INCREASE THE COUNT
+      </button>
+      <button
+        className="btn"
+        onClick={() => {
+          sub();
+        }}
+      >
+        DECREASE THE COUNT
+      </button>
+      <button className="btn" onClick={reset}>
+        RESET THE COUNT
+      </button>
       {/* whens using state...we have to put setCount inside a function and not use it */}
       {/* - The hooks are non-persistant in React functional components...means that if the user refreshes or leaves the the page then the useState variable gets to the default value...the state value is not saved by default...it can be only achieved by LocalStorage concept or retrieve from database. */}
     </>
-   )
-};
+  );
+}
 export default App;
 
 // - If use the component multiple time...then updation in 1 component won't effect the other components...it means that states are not shared..they are independent.
